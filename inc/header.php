e52b9aeb43a147b025842b039908b96f9d335eaf
ViniciusVRodrigues <96784778+ViniciusVRodrigues@users.noreply.github.com>
2024-03-18 10:43:20 -0300

    header separado

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -42,35 +41,3 @@
             </div>
         </div>
     </header>
-<!-- parte principal da página-->
-
-    <main>
-        <section class="topo-do-site">
-            <div class="interface">
-                <div class="flex">
-                    <div class="txt-topo-site">
-                        <h1>Safe</h1><br>
-                        <h2>Dish</h2>
-                    </div>
-
-                    <div class="img-topo-site">
-                        <img src="img/Vector.png" alt="img logo">
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="especialidades">
-            <div class="interface">
-                <h2 class="título">Você <span>Sabia?</span></h2>
-                <div class="flex">
-                    <div class="especialidades-box">
-                        <i class="bi bi-code-square"></i>
-                        <h3>Você Sabia?</h3>
-                        <p>A pessoa celíaca é aquela portadora de uma doença autoimune, ou seja, um mau funcionamento no sistema imunológico que leva o corpo a atacar ele mesmo. Pessoas que possuem essa doença não podem consumir glúten (composto de proteínas que fica em alimentos com cevada, trigo e centeio). Essa patologia tem níveis - enquanto algumas pessoas devem apenas evitar o glúten, outras não podem nem mesmo estar no mesmo ambiente que um alimento deste. Por isso, elas acabam cozinhando em casa e raramente tem a experiência de comer socialmente. Além disso, o percentual de pessoas celíacas no Brasil, é de 1%, tornando difícil encontrar restaurantes inclusivos dessa restrição.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
-</body>
-</html>
