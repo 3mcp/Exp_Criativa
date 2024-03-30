88f2757620abca266391d5437e0481a72b9175c5
kaka-ubial <karen.ubial@pucpr.edu.br>
2024-03-30 16:32:11 -0300

    commit restaurante

--- databaseRestaurante/restauranteDeletar.php
+++ databaseRestaurante/restauranteDeletar.php
@@ -4,12 +4,12 @@ include("dbconnection/functions.php");
 $usuarioID   = $conn->real_escape_string($_SESSION['ID']); 
 
 $tabela = "restaurante";
-$condicao = "IdRestaurante = ".$usuarioID;
+$condicao = "IdRestaurante = ".$restauranteID;
 
 echo deleteByCondition($conn, $tabela, $condicao);
 
 if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])){
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 } else {
-    header('Location: usuarioTeste.php');
+    header('Location:');
 }
---
+++ databaseRestaurante/restauranteLogar.php
@@ -0,0 +1,21 @@
+<?php
+include("../dbconnection/functions.php");
+
+$email = $conn->real_escape_string($_POST['EmailRestaurante']); // prepara a string recebida para ser utilizada em comando SQL
+$senha   = $conn->real_escape_string($_POST['SenhaRestaurante']); // prepara a string recebida para ser utilizada em comando SQL
+
+$tabela = "restaurante";
+    $aCampos = ["IdRestaurante","EmailRestaurante","SenhaRestaurante"];
+    $condicao = "EmailRestaurante = ".$email." AND SenhaRestaurante = ".md5($senha);
+
+    $usuarios = select($conn, $aCampos, $tabela, $condicao);
+    foreach($usuarios as $usuario){
+        session_start();
+        $_SESSION["ID"] = $usuario["IdRestaurante"];
+    }
+
+if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])){
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+} else {
+    header('Location:');
+}
