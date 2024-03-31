641cefecc65471a2e6c0cf0e5564c98c2f64e4e0
unknown <you@example.com>
2024-03-31 18:55:23 -0300

    nav e slogan finalizados

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 
 
     <main>
-        <div>
-            
+        <div class='container'>
+            <div class="nomeDoSite">
+                <h1>Safe</h1>
+                <h2>Dish</h2>
+            </div>
+            <div class="vetor">
+                <img src="img/Vector.png" alt="" class='vetor'>
+            </div>
         </div>
     </main>
 </body>
