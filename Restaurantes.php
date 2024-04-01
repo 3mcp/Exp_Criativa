5fb903d294429d4f3c0453933b97dfb6f3854fc5
Murilo Carvalho Pereira <137966397+3mcp@users.noreply.github.com>
2024-04-01 18:52:02 -0300

    restaurantes

--- Restaurantes.php
+++ Restaurantes.php
@@ -9,19 +9,24 @@
             <input type="text" id="search-bar" placeholder="Busca">
             <a href="#"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"></a>
         </form>
-    </div>
     
-    <section class="Restaurantes">
-            <div class="interface-restaurante">
-                <div class="flex">
-                    <div class="" style="">
-                        <div class="overlay">Projeto 1</div>
-                    </div>
-                    <div class="img-port" style="">
-                        <div class="overlay">Projeto 2</div>
-                    </div>
-                </div>
+        
+        <section class="Restaurantes">
+            <div class="restaurante">
+                <img src="img/fachada (teste).jpg" alt="">
+                <p>Swadisht</p>
+                <h5>Restaurante indiano elegante que combina pratos e vinho, com destaque para o chana masala e as samosas.</h5>
+                <button>Ver mais</button>
+                <i class="bi bi-shop"></i>
+            </div>
+            <div class="restaurante">
+                <img src="img/fachada2 (teste).jpg" alt="">
+                <p>Zapata</p>
+                <h5>Bar colorido e animado com comida e música mexicanas e pista de dança aberta para iniciantes e profissionais.</h5>
+                <button>Ver mais</button>
+                <i class="bi bi-shop"></i>
             </div>
         </section>
+    </div>
 </body>
 </html>
