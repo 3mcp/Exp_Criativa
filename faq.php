a44bfef912968d5dbb0e7f192c37e449ca0a2323
natimolini <nati.buffa1907@gmail.com>
2024-04-18 11:58:07 -0300

    .

--- faq.php
+++ faq.php
@@ -12,13 +12,54 @@
 
         <div class="topicos">
             <div class="topico">
-                <h1>Duvidas frequentes</h1>
-                <p>tudo sobre as duvidas frequentes </p>
+                <h1>Como faço para me cadastrar?</h1>
+                <p>Para se cadastrar, basta clicar no botão "Registre-se" no canto superior direito da tela e preencher os campos solicitados. </p>
             </div>
 
             <div class="topico">
-                <h1>Banimento e suspensões</h1>
-                <p>tudo sobre o Banimento</p>
+                <h1>Como faço para me logar?</h1>
+                <p>Para se logar, basta clicar no botão "Entrar" no canto superior direito da tela e preencher os campos solicitados.</p>
+            </div>
+
+            <div class="topico">
+                <h1>Como faço para encontrar restaurantes?</h1>
+                <p>Para encontrar restaurantes, basta clicar em restaurantes no menu superior da tela e escolher o restaurante desejado.</p>
+            </div>
+
+            <div class="topico">
+                <h1>Como faço para encontrar mais informações sobre a doença celíaca?</h1>
+                <p>Para encontrar mais informações sobre a doença celíaca, basta clicar em home no menu superior da tela. Porém se deseja saber mais afundo aconselhamos procurar um médico gastro especializado nessa doença.
+                </p>
+            </div>
+            
+            <div class="topico">
+                <h1>Como faço para comentar sobre um restaurante?</h1>
+                <p>Essa versão não possui a possibilidade de comentar em restaurantes.
+                </p>
+            </div>
+            <div class="topico">
+                <h1>Como faço para encontrar pratos com a minha categoria?</h1>
+                <p>Essa versão não possui a possibilidade encontrar pratos por categoria.
+                </p>
+            </div>
+            <div class="topico">
+                <h1>O que é uma categoria no Safe Dish?</h1>
+                <p>As categorias no Safe Dish são separadas em categorias como doença celíaca, intolerante a lactose, vegano, entre outros. Para poder relacionar os pratos dos restaurantes que condizem com a sua categoria. Ou seja tanto nossos usuários quanto os pratos do restaurante possuem uma categoria.
+            </div>
+            <div class="topico">
+                <h1>Como faço para adicionar a minha categoria no site?</h1>
+                <p>Essa versão não possui a possibilidade de adicionar categoria.
+                </p>
+            </div>
+            <div class="topico">
+                <h1>Como faço para denunciar um comentário?</h1>
+                <p>Essa versão não possui a possibilidade de denunciar um comentário.
+                </p>
+            </div>
+            <div class="topico">
+                <h1>Como faço para adicionar pratos no meu restaurante?</h1>
+                <p>Essa versão não possui a possibilidade de adicionar pratos ao seu restaurante.
+                </p>
             </div>
 
         </div>
