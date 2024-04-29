4e8020b0ecd9c7c7fc77142ee9ed3c945c48d49b
kaka-ubial <karen.ubial@pucpr.edu.br>
2024-04-29 09:36:19 -0300

    directory-change

--- inc/cadastroUsuario.php
+++ inc/cadastroUsuario.php
@@ -66,47 +66,5 @@
 
     <script src="../js/cadastro.js"></script>
     <script src="../js/mascarasUsuarios.js"></script>
-
-    <script>
-        function trim(str) {
-            return str.replace(/^\s+|\s+$/g, '');
-        }
-
-        function validateForm() {
-            var namePRAValue = document.getElementById("inputNome").value.trim();
-            var namePRAError = document.getElementById("namePRAError");
-            var emailPRAValue = document.getElementById("inputEmail").value;
-            var passwordPRAValue = document.getElementById("inputPassword").value;
-            var emailPRAError = document.getElementById("emailPRAError");
-            var passwordPRAError = document.getElementById("passwordPRAError");
-
-            if (namePRAValue.split(' ').length < 2) {
-                namePRAError.textContent = "Insira seu nome completo";
-                return false;
-            } else {
-                namePRAError.textContent = "";
-            }
-
-            var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-            if (!emailPattern.test(emailPRAValue)) {
-                emailPRAError.textContent = "Por favor, insira um email válido.";
-                return false; 
-            } else {
-                emailPRAError.textContent = ""; 
-            }
-
-            var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-            if (!passwordPattern.test(passwordPRAValue)) {
-                passwordPRAError.innerHTML = "A senha deve ter no mínimo:<br>* 8 caracteres<br>* Uma letra maiúscula<br>* Um número<br>* Um caractere especial (@, $, !, %, *, ?, &).";
-                return false; 
-            } else {
-                passwordPRAError.textContent = ""; 
-            }
-
-            return true; 
-        }
-    </script>
 </body>
 </html>
--- js/cadastro.js
+++ js/cadastro.js
@@ -10,3 +10,43 @@ restaurantBtn.addEventListener("click", function () {
 goBackBtn.addEventListener("click", function () {
   window.location.href = "../index.php";
 });
+
+function trim(str) {
+  return str.replace(/^\s+|\s+$/g, '');
+}
+
+function validateForm() {
+  var namePRAValue = document.getElementById("inputNome").value.trim();
+  var namePRAError = document.getElementById("namePRAError");
+  var emailPRAValue = document.getElementById("inputEmail").value;
+  var passwordPRAValue = document.getElementById("inputPassword").value;
+  var emailPRAError = document.getElementById("emailPRAError");
+  var passwordPRAError = document.getElementById("passwordPRAError");
+
+  if (namePRAValue.split(' ').length < 2) {
+      namePRAError.textContent = "Insira seu nome completo";
+      return false;
+  } else {
+      namePRAError.textContent = "";
+  }
+
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(emailPRAValue)) {
+      emailPRAError.textContent = "Por favor, insira um email válido.";
+      return false; 
+  } else {
+      emailPRAError.textContent = ""; 
+  }
+
+  var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passwordPattern.test(passwordPRAValue)) {
+      passwordPRAError.innerHTML = "A senha deve ter no mínimo:<br>* 8 caracteres<br>* Uma letra maiúscula<br>* Um número<br>* Um caractere especial (@, $, !, %, *, ?, &).";
+      return false; 
+  } else {
+      passwordPRAError.textContent = ""; 
+  }
+
+  return true; 
+}
