264a9e04add6a929e94f1a0f105bb405a5472386
Your Name <you@example.com>
2024-05-06 11:22:38 -0300

    tela de add e editar prato adicionada

--- cardapio.php
+++ cardapio.php
@@ -6,11 +6,19 @@
         <p>Cardápio</p>
         <button class='addBtn' onclick="on('form')">+</button>
         <div class="overlay" id='form'>
-            <div class='addPratoForm'>
-                <h1>Adicione um prato</h1>
-                <form action="">
+            <div class='addPrato'>
+                <div>
+                    <h1>Adicione um prato</h1>
+                    <i class="bi bi-x" onclick="off('form')"></i>
+                </div>
+                <form action="" class='addPratoForm'>
                     <label for="nomePrato">Nome do prato</label>
-                    <input type="text" id='nomePrato'>
+                    <input type="text" id='nomePrato' placeholder='Ex: Macarrão com Salsicha '>
+                    <label for="precoPrato">Preço do Prato</label>
+                    <input type="text" id='precoPrato' placeholder='Ex: R$00.00'>
+                    <label for="descricaoPrato">Descrição do prato</label>
+                    <textarea id='descricaoPrato' maxlength="400" placeholder="Ex: uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano..."></textarea>
+                    <button class='enviarBtn'>Enviar</button>
                 </form>
             </div>
         </div>
@@ -18,8 +26,26 @@
             <div class="cardapio-item">
                 <img src='img/fotoPrato.png'>
                 <div class='cardapio-info'>Entrecot alla Fonduta di Formaggio
-com Tagliatele</div>
-                <button class='saibaMaisBtn' onclick="on('entrecot')">Saiba mais</button>
+                com Tagliatele <i class="bi bi-pencil-square" onclick="on('editForm')"></i></div>
+                <div class="overlay" id='editForm'>
+                    <div class='editPrato'>
+                        <div>
+                            <h1>Editar</h1>
+                            <i class="bi bi-x" onclick="off('editForm')"></i>
+                        </div>
+                        <form action="" class='editPratoForm'>
+                            <label for="nomePrato">Nome do prato</label>
+                            <input type="text" id='nomePrato' placeholder='Ex: Macarrão com Salsicha '>
+                            <label for="precoPrato">Preço do Prato</label>
+                            <input type="text" id='precoPrato' placeholder='Ex: R$00.00'>
+                            <label for="descricaoPrato">Descrição do prato</label>
+                            <textarea id='descricaoPrato' maxlength="400" placeholder="Ex: uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano..."></textarea>
+                            <button class='enviarBtn'>Salvar</button>
+                        </form>
+                    </div>
+                    </div>
+
+                    <button class='saibaMaisBtn' onclick="on('entrecot')">Saiba mais</button>
                 <div class="overlay" id='entrecot' onclick="off()">
                 <div class="cardapio-info-prato">
                     <div class='cardapio-info-texto'>
@@ -30,77 +56,7 @@ com Tagliatele</div>
                     <button class='fecharBtn' onclick="off('entrecot')">Fechar</button>
                 </div></div>
             </div>
-            <div class="cardapio-item">
-                <img src='img/burrata.png'>
-                <div class='cardapio-info'>Burrata ao Pesto Rosso</div>
-                <button class='saibaMaisBtn' onclick="on('burrata')">Saiba mais</button>
-                <div class="overlay" id='burrata' onclick="off()">
-                <div class="cardapio-info-prato">
-                    <div class='cardapio-info-texto'>
-                        <h1>Burrata ao Pesto Rosso</h1>
-                        <img src="./img/burrata.png" alt="">
-                    </div>
-                    <p>Uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano suave e cremoso, com a intensidade do tomate seco, resultando em uma combinação irresistível. Adicionamos uma pitada de manjericão fresco para adicionar um toque aromático e refrescante.</p>
-                    <button class='fecharBtn' onclick="off('burrata')">Fechar</button>
-                </div></div>
-            </div>
-            <div class="cardapio-item">
-                <img src='img/bruschetta.png'>
-                <div class='cardapio-info'>Bruschetta burrata, tomate seco e manejericão</div>
-                <button class='saibaMaisBtn' onclick="on('bruschetta')">Saiba mais</button>
-                <div class="overlay" id='bruschetta' onclick="off()">
-                <div class="cardapio-info-prato">
-                    <div class='cardapio-info-texto'>
-                        <h1>Bruschetta burrata, tomate seco e manejericão</h1>
-                        <img src="./img/bruschetta.png" alt="">
-                    </div>
-                    <p>Uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano suave e cremoso, com a intensidade do tomate seco, resultando em uma combinação irresistível. Adicionamos uma pitada de manjericão fresco para adicionar um toque aromático e refrescante.</p>
-                    <button class='fecharBtn' onclick="off('bruschetta')">Fechar</button>
-                </div></div>
-                
-            </div>
-            <div class="cardapio-item">
-                <img src='img/tagliatta.png'>
-                <div class='cardapio-info'>Tagliata de Entrecot Alla Cacciatora com Tagliatele</div>
-                <button class='saibaMaisBtn' onclick="on('tagliata')">Saiba mais</button>
-                <div class="overlay" id='tagliata' onclick="off()">
-                <div class="cardapio-info-prato">
-                    <div class='cardapio-info-texto'>
-                        <h1>Tagliata de Entrecot Alla Cacciatora com Tagliatele</h1>
-                        <img src="./img/tagliatta.png" alt="">
-                    </div>
-                    <p>Uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano suave e cremoso, com a intensidade do tomate seco, resultando em uma combinação irresistível. Adicionamos uma pitada de manjericão fresco para adicionar um toque aromático e refrescante.</p>
-                    <button class='fecharBtn' onclick="off('tagliata')">Fechar</button>
-                </div></div>
-
-            </div>
-            <div class="cardapio-item">
-                <img src='img/fillet.png'>
-                <div class='cardapio-info'>Filetto al Pepe Verde com Tagliatele</div>
-                <button class='saibaMaisBtn' onclick="on('filetto')">Saiba mais</button>
-                <div class="overlay" id='filetto' onclick="off()">
-                <div class="cardapio-info-prato">
-                    <div class='cardapio-info-texto'>
-                        <h1>Filetto al Pepe Verde com Tagliatele</h1>
-                        <img src="./img/fillet.png" alt="">
-                    </div>
-                    <p>Uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano suave e cremoso, com a intensidade do tomate seco, resultando em uma combinação irresistível. Adicionamos uma pitada de manjericão fresco para adicionar um toque aromático e refrescante.</p>
-                    <button class='fecharBtn' onclick="off('filetto')">Fechar</button>
-                </div></div>
-            </div>
-            <div class="cardapio-item">
-                <img src='img/burrata2.png'>
-                <div class='cardapio-info'>Burrata com Cebola Caramelizada</div>
-                <button class='saibaMaisBtn' onclick="on('burrata2')">Saiba mais</button>
-                <div class="overlay" id='burrata2' onclick="off()">
-                <div class="cardapio-info-prato">
-                    <div class='cardapio-info-texto'>
-                        <h1>Burrata com Cebola Caramelizada</h1>
-                        <img src="./img/burrata2.png" alt="">
-                    </div>
-                    <p>Uma entrada que é uma autêntica explosão de sabores. Combinamos a riqueza da burrata, um queijo italiano suave e cremoso, com a intensidade do tomate seco, resultando em uma combinação irresistível. Adicionamos uma pitada de manjericão fresco para adicionar um toque aromático e refrescante.</p>
-                    <button class='fecharBtn' onclick="off('burrata2')">Fechar</button>
-                </div></div>
+            
 
             </div>
         </div>
