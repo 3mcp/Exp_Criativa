f0c3248275be2b98165a4f94fea9382128c2c3f0
Your Name <you@example.com>
2024-05-16 10:40:30 -0300

    comentarios restaurante

--- Restaurante.php
+++ Restaurante.php
@@ -3,13 +3,41 @@
 <main>
     <div class='restaurante-pg'>
         <h1 class='restaurante-titulo'>Restaurante</h1>
-        <img src='img/pngDefault.png' class='restaurante-imagem'>
+        <div class="restaurante-imagem-container">
+            <img src='img/pngDefault.png' class='restaurante-imagem'>
+        </div>
         <div class='restaurante-info'>
             <p>Endereço: Rua Polenta Brasil, 123 </p>
             <p>Site: www.polentabrasil.com.br</p>
         </div>
         <button class='restaurante-botao-cardapio'>Visualizar Cardápio</button>
+        <div class="comentario-container">
+
+            <h2>Comentários</h2>
+            <hr>
+            <div class="comentario-box-escreva comentario-box">
+                <h3>Escreva um comentário:</h3>
+                <div class='comentarioEscrevaDiv'>
+                    <textarea placeholder='Escreva aqui o que achou do nosso restaurante...'></textarea>
+                    <button class='comentarioEnviarBtn'>Enviar</button>
+                </div>
+            </div>
+            <div class="comentario-box">
+                <div class="comentario-info">
+                    <img src="./img/profilepic.png" class='comentarioProfilePic' alt="">
+                    <div>
+                        <p style="font-weight: bold;">Nome do usuário</p>
+                        <p>Comentou em: 10/10/2010</p>
+                    </div>
+                </div>
+                <div class='comentario-content'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt atque quia nulla quaerat blanditiis sequi qui consectetur accusamus, iusto, quam officia odit fugiat dolores vel? Incidunt aperiam fuga veritatis. Illo. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure dolor minima error reiciendis magni dolore, optio nobis pariatur nam vel provident? Hic dignissimos incidunt odio alias doloribus. Hic, sed asperiores!</p>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 </main>
 
 <?php include("inc/footer.php") ?>
--- js/faq.js
+++ js/faq.js
@@ -1,42 +1,42 @@
 function searchTopics() {
-    const searchBar = document.getElementById('search-bar');
-    const searchTerm = searchBar.value.toLowerCase();
-    const topicos = document.querySelectorAll('.topico');
+  const searchBar = document.getElementById("search-bar")
+  const searchTerm = searchBar.value.toLowerCase()
+  const topicos = document.querySelectorAll(".topico")
 
-    topicos.forEach(topico => {
-        const titulo = topico.querySelector('h1').textContent.toLowerCase();
-        const paragrafo = topico.querySelector('p').textContent.toLowerCase();
-        if (titulo.includes(searchTerm) || paragrafo.includes(searchTerm)) {
-            topico.style.display = 'block';
-        } else {
-            topico.style.display = 'none';
-        }
-    });
+  topicos.forEach((topico) => {
+    const titulo = topico.querySelector("h1").textContent.toLowerCase()
+    const paragrafo = topico.querySelector("p").textContent.toLowerCase()
+    if (titulo.includes(searchTerm) || paragrafo.includes(searchTerm)) {
+      topico.style.display = "block"
+    } else {
+      topico.style.display = "none"
+    }
+  })
 }
 
 // Adicionando evento de input à barra de pesquisa
-document.addEventListener('DOMContentLoaded', function() {
-    const searchBar = document.getElementById('search-bar');
-    searchBar.addEventListener('input', searchTopics);
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("search-bar")
+  searchBar.addEventListener("input", searchTopics)
+})
 
 // Obtém todas as perguntas e respostas
-const perguntas = document.querySelectorAll('.pergunta');
-const respostas = document.querySelectorAll('.resposta');
+const perguntas = document.querySelectorAll(".pergunta")
+const respostas = document.querySelectorAll(".resposta")
 
 // Adiciona um evento de clique a cada pergunta
 perguntas.forEach((pergunta, index) => {
-    pergunta.addEventListener('click', () => {
-        // Verifica se a resposta está visível
-        const respostaAtual = respostas[index];
-        const estaVisivel = respostaAtual.style.display === 'block';
+  pergunta.addEventListener("click", () => {
+    // Verifica se a resposta está visível
+    const respostaAtual = respostas[index]
+    const estaVisivel = respostaAtual.style.display === "block"
 
-        // Oculta todas as respostas
-        respostas.forEach(resposta => {
-            resposta.style.display = 'none';
-        });
+    // Oculta todas as respostas
+    respostas.forEach((resposta) => {
+      resposta.style.display = "none"
+    })
 
-        // Mostra a resposta clicada se estiver oculta, oculta se estiver visível
-        respostaAtual.style.display = estaVisivel ? 'none' : 'block';
-    });
-});
+    // Mostra a resposta clicada se estiver oculta, oculta se estiver visível
+    respostaAtual.style.display = estaVisivel ? "none" : "block"
+  })
+})
