142c1a889c54d2e15023c6241b27a8b1b51821cb
kaka-ubial <karen.ubial@pucpr.edu.br>
2024-05-20 09:29:33 -0300

    dump atualizado

--- dbconnection/dump/safedish_p_r_a_.sql
+++ dbconnection/dump/safedish_p_r_a_.sql
@@ -33,7 +33,7 @@ CREATE TABLE `p_r_a_` (
   PRIMARY KEY (`IdPRA`),
   UNIQUE KEY `UsernamePRA` (`UsernamePRA`),
   UNIQUE KEY `EmailPRA` (`EmailPRA`)
-) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-20  9:18:49
+-- Dump completed on 2024-05-20  9:29:19
